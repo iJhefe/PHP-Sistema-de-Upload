92b5e8aefc5afb353e7bfd9489aeb526b8fb4a65
Jeffyter Saraiva <jeffytersaraiva@gmail.com>
2020-04-11 20:40:21 -0300

    Adicionado busca de arquivos .css.php
    Agora é possível manipular css com php

--- application/Hook/CSSFile.php
+++ application/Hook/CSSFile.php
@@ -11,13 +11,18 @@ class CSSFile
 
     public function __construct(string $name)
     {
-        $path = Path::get_real_path('{base}/src/styles/' . $name . '.css');
+        $path = Path::get_real_path('{base}/src/styles/' . $name);
 
         header('Content-Type: text/css');
 
-        if (file_exists($path))
-            include $path;
-        else
+        if (file_exists($path . '.css')) {
+            include $path . '.css';
+        }
+        else if (file_exists($path . '.css.php')) {
+            include $path . '.css.php';
+        }
+        else {
             echo '/* Não encontrado */';
+        }
     }
 }
